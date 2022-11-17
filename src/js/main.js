e01c57d766ac448231bacca184ea6c1663e3ff52
kumofff <kumofff@gmail.com>
2022-11-17 12:27:17 +0300

    some added

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-//here will by JavaScript code ...
+$(document).ready(function(){
+    $('.slider-catalog').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1
+    });
+    $('.slider-reviews').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots:true,
+      arrows:false
+    });
+  });
