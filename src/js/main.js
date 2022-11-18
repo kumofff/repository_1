a279450b61d187f0a77d0c8af279ac4cf613894f
kumofff <kumofff@gmail.com>
2022-11-18 14:05:43 +0300

    responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
     $('.slider-catalog').slick({
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToScroll:1,
+      responsive:[
+        {
+          breakpoint:840,
+          settings:{
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint:664,
+          settings:{
+            slidesToShow: 1
+          }
+        }
+      ]
     });
     $('.slider-reviews').slick({
       slidesToShow: 1,
